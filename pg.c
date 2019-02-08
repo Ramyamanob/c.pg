ce269d556ba1b517d79f4afcecb1e249a1d6a658
Ramyamanob <47448974+Ramyamanob@users.noreply.github.com>
2019-02-08 18:49:17 +0530

    Update and rename README.md to pg.c

---
+++ pg.c
@@ -0,0 +1,17 @@
+# c.pg
+#include<stdio.h>
+int main()
+{
+ char ch;
+ scanf("%c",&ch);
+ if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z')
+ 
+ {
+     printf("alphabet");
+ }
+ else
+ {
+     printf("no");
+ }
+ return 0;
+}
